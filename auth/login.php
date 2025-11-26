ef0ceb07f5f59e102be3212fef0190837fe36918
Manoj Gautam <manojit.gautam@gmail.com>
2025-11-26 08:05:00 +0545

    Update login.php with dashboard

--- auth/login.php
+++ auth/login.php
@@ -1,4 +1,18 @@
 <?php 
+ // Start session to store logged in user information 
+ // so that the user information is available accross the pages 
+
+ session_start();
+
+ echo var_dump($_SESSION);
+
+ // if user is already logged in redirect him to dashboard
+ if(isset($_SESSION["username"])) {
+
+            header("Location: ../dashboard/dashboard.php");
+ }
+
+
  //1 . Import $conn 
 include("../config/db_config.php");
     
@@ -16,20 +30,84 @@ include("../config/db_config.php");
 // the data from login form
 if($_SERVER["REQUEST_METHOD"] == "POST") {
   
-    $username = $_POST["username"]; // username is the name of input field in html name must match
-    $password = $_POST["password"]; // name defined in password field
+    $form_username = $_POST["username"]; // username is the name of input field in html name must match
+    $form_password = $_POST["password"]; // name defined in password field
 
+    if(empty($form_username) || empty($form_password)) {
+        die("Please provide username or password");
+    }
+
+   
+    // If form fields are not empty start to fetch from db and compare database value 
+    // and form value
     
     $stmt = $conn->prepare("SELECT username, password FROM users WHERE username = ? AND password = ?");
-    $stmt->bind_param("ss", $username, $password);
+    $stmt->bind_param("ss", $form_username, $form_password);
+
     $stmt->execute();
+
+
 
     $result = $stmt->get_result();
 
-    echo var_dump($result);
+
+    if($result->num_rows === 1) {
+        // username and password exist 
+        // login user and reirect it ddashboard 
+
+        echo $result->num_rows;
+
+        $record = $result->fetch_assoc(); // pull record from db into $record variable 
+        
+        $db_username = $record["username"];
+        $db_password = $record["password"];
+
+       
+
+        if($form_username==$db_username && $form_password  == $db_password) {
+            // Do Login 
+            
+            $_SESSION["username"] = $db_username;
+            $_SESSION["id"] = $record["id"];
+
+            // session is seted now send this user to dashboard
+            header("Location: ../dashboard/dashboard.php");
+
+            
+
+        } else {
+            die("Your username or password is mismached");
+        }
+
+
+        
+    }
 
 
 
+
+
+
+
+
+   
+    // if ($result->num_rows === 1) {
+    // $row = $result->fetch_assoc();
+
+    // // Verify hashed password
+    // if (password_verify($password, $row['password'])) {
+
+    //     // Set session variables
+    //     $_SESSION["user_id"] = $row["id"];
+    //     $_SESSION["username"] = $row["username"];
+    //     $_SESSION["logged_in"] = true;
+
+    //     echo "Login successful!";
+    //     header("Location: dashboard.php");
+    //     exit();
+    // } else {
+    //     echo "Invalid password";
+    // }
 
 }
 
@@ -62,13 +140,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 
-<h1> Login  </h1>
-<div id="login-form"> 
-<form method='POST' action="./login.php"> 
-    Username: <input type="text" name="username" /> <br/>
-    Password: <input type="text" name="password" /> <br/> <br/>
-    <input type="submit" value="Login" />
+<h1> Login </h1>
+<div id="login-form">
+    <form method='POST' action="./login.php">
+        Username: <input type="text" name="username" /> <br />
+        Password: <input type="text" name="password" /> <br /> <br />
+        <input type="submit" value="Login" />
 
-</form>
+    </form>
 
 </div>
