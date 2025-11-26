87c953c0671a4158d894af23c51868285192e7eb
Manoj Gautam <manojit.gautam@gmail.com>
2025-11-26 08:19:02 +0545

    updated logout

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -1,8 +1,14 @@
 <?php 
 session_start();
 
-
 echo "Welcome".$_SESSION["username"];
 
 
+
 ?>
+
+<h1>Click here to logout</h1>
+<form method="POST" action="../auth/logout.php">
+    <input type="submit" value="logout">
+
+</form>
