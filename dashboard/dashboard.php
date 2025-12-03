e821454a7f823b9180d7b38062c7f11933bd12f1
Manoj Gautam <manojit.gautam@gmail.com>
2025-12-03 07:50:39 +0545

    Added helper folder to organize code better

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -1,23 +1,10 @@
-<?php 
-session_start();
+<?php  
+// check for user login 
+// only allow this page if user is logged in 
+// logged in user is check using the logic inside ../helper/check_login.php
+// to use this code , we need to inclue here using include function
 
-// If user is not logged in 
-// means tthere is not username value in $_SESSION variable
-// we are checking this 
-
-// if user le login garako cha vane $isUserLoggedIn ma username hunu parcha
-// if user le login garako chaina vane hunu hudina 
-
-$isUserLoggedIn = isset($_SESSION["username"]);
-
-// if user logged in chaina vane 
-// Login page ma pathaidene
-// ! le chai opposite value dincha true cha vane false and false cha vane true
-if( !$isUserLoggedIn) {
-     header("Location: ../auth/login.php");
-     exit(); // exit this page and go to login.php
-
-} 
+include('../helper/check_login.php');
 
 ?>
 
@@ -28,6 +15,7 @@ if( !$isUserLoggedIn) {
 3. page protection completed
 4. New record addition using html form completed 
 5. query data and display result in html page also completed 
+
 
 <h1>Click here to logout</h1>
 <form method="POST" action="../auth/logout.php">
---
+++ helper/check_login.php
@@ -0,0 +1,22 @@
+<?php 
+session_start();
+
+// If user is not logged in 
+// means tthere is not username value in $_SESSION variable
+// we are checking this 
+
+// if user le login garako cha vane $isUserLoggedIn ma username hunu parcha
+// if user le login garako chaina vane hunu hudina 
+
+$isUserLoggedIn = isset($_SESSION["username"]);
+
+// if user logged in chaina vane 
+// Login page ma pathaidene
+// ! le chai opposite value dincha true cha vane false and false cha vane true
+if( !$isUserLoggedIn) {
+     header("Location: ../auth/login.php");
+     exit(); // exit this page and go to login.php
+
+} 
+
+?>
